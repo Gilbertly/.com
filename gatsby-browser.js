6eb9388524e56d4c79afb415524cac81178f0748
Gilbertly <gilbert@qubitping.com>
2020-02-08 13:48:50 +0300

    feat: init gatsby config

---
+++ gatsby-browser.js
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import './src/styles/index.less';
+
+export const onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(`
+    This application has been updated.
+    Reload to display the latest version?
+  `);
+  if (answer === true) {
+    window.location.reload();
+  }
+};
---
+++ gatsby-config.js
@@ -0,0 +1,34 @@
+/* eslint-disable @typescript-eslint/camelcase */
+module.exports = {
+  siteMetadata: {
+    title: 'Gilbertly.com',
+  },
+  plugins: [
+    'gatsby-plugin-typescript',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Gilbertly.com',
+        short_name: 'Gilbertly.com',
+        display: 'standalone',
+        start_url: '/',
+        background_color: '#e6f7ff',
+        theme_color: '#e6f7ff',
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-less',
+      options: {
+        javascriptEnabled: true,
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'assets',
+        path: '${__dirname}/src/assets',
+      },
+    },
+    'gatsby-plugin-react-helmet',
+  ],
+};
