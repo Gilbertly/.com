ba231c3c6a977a373ce42151016763420c8ee098
Gilbertly <gilbert@qubitping.com>
2020-02-08 13:46:52 +0300

    feat: init intro header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import { Button } from 'antd';
+
+export const Header = ({ avatarSvg }: any) => {
+  return (
+    <section className="section-header">
+      <div className="avatar">
+        <img src={avatarSvg} alt="Gilbert Gathara" />
+      </div>
+      <div className="content">
+        <h2>
+          <b>
+            {"Hi, I'm Gilbert Gathara"}
+            <span role="img" aria-label="wave">
+              👋🏽
+            </span>
+          </b>
+        </h2>
+        <h3>
+          I collaborate with data and dev teams to build, scale, and keep
+          infrastructure healthy for production apps & machine learning models.
+        </h3>
+        <Button type="primary" icon="download">
+          Download Resume
+        </Button>
+      </div>
+    </section>
+  );
+};
