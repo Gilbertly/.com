e1e4ec2080e772c75f1fb69693aac533111c70b6
Gilbertly <gilbert@qubitping.com>
2020-02-08 13:56:15 +0300

    fix: resolve string interpolation

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'assets',
-        path: '${__dirname}/src/assets',
+        path: `${__dirname}/src/assets`,
       },
     },
     'gatsby-plugin-react-helmet',
