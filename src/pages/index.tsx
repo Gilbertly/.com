dd3d52fa06169ea9937af45ebe37bbc33916b576
Gilbertly <gilbert@qubitping.com>
2020-02-11 16:52:05 +0300

    refactor: resolve avatar naming & add react-helmet config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { graphql } from 'gatsby';
+import { Helmet } from 'react-helmet';
 
 import { Header } from '../components/Header';
 import { Socials } from '../components/Socials';
@@ -8,7 +9,7 @@ import { BackendSkillset } from '../components/BackendSkillset';
 import { Footer } from '../components/Footer';
 
 const IndexPage = (props: any) => {
-  const avatarSvg = props.data.avatarSvg.publicURL;
+  const avatarProfile = props.data.avatarProfile.childImageSharp.fluid.src;
   const socialIcons = props.data.socialIcons.edges;
   const frontendIcons = props.data.frontendIcons.edges;
   const backendIcons = props.data.backendIcons.edges;
@@ -16,7 +17,16 @@ const IndexPage = (props: any) => {
 
   return (
     <div className="page-index">
-      <Header avatarSvg={avatarSvg} />
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Gilbert Gathara</title>
+        <meta
+          name="Gilbert Gathara"
+          content="Software/Data Engineering Portfolio"
+        />
+        <link rel="canonical" href="https://gilbertly.com/" />
+      </Helmet>
+      <Header avatarProfile={avatarProfile} />
       <Socials socialIcons={socialIcons} />
       <FrontendSkillset frontendIcons={frontendIcons} />
       <BackendSkillset backendIcons={backendIcons} awsIcons={backendAWSIcons} />
@@ -29,8 +39,12 @@ export default IndexPage;
 
 export const pageQuery = graphql`
   query IndexPageQuery {
-    avatarSvg: file(relativePath: { eq: "img/avatarProfile.svg" }) {
-      publicURL
+    avatarProfile: file(relativePath: { eq: "img/avatarProfile.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 112, maxHeight: 112) {
+          src
+        }
+      }
     }
     socialIcons: allFile(filter: { absolutePath: { regex: "/(social-)/" } }) {
       edges {
