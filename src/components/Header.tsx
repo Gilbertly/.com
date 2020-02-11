ca8bbb4abd9f27ee4be7fcc7b9cd327c65e93b26
Gilbertly <gilbert@qubitping.com>
2020-02-11 12:09:17 +0300

    chore: update wording & resume request

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,10 +18,11 @@ export const Header = ({ avatarSvg }: any) => {
         </h2>
         <h3>
           I collaborate with data and dev teams to build, scale, and keep
-          infrastructure healthy for production apps & machine learning models.
+          infrastructure healthy for production apps & machine learning models
+          on AWS.
         </h3>
-        <Button type="primary" icon="download">
-          Download Resume
+        <Button type="primary" icon="file-pdf">
+          Request Resume
         </Button>
       </div>
     </section>
