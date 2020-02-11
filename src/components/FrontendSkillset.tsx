e0638703ec2e4ab9e7daa5928b24c0f09409e827
Gilbertly <gilbert@qubitping.com>
2020-02-11 12:35:35 +0300

    chore: add the rest of my frontend skills

--- src/components/FrontendSkillset.tsx
+++ src/components/FrontendSkillset.tsx
@@ -13,6 +13,13 @@ export const FrontendSkillset = ({ frontendIcons }: any) => {
   frontendIcons.map((frontendIcon: any) => {
     const iconURL = frontendIcon.node.childImageSharp.fluid.src;
 
+    if (iconURL.includes('fe-figma')) {
+      skills.push({
+        name: 'Figma',
+        url: iconURL,
+        alt: 'Figma Icon',
+      });
+    }
     if (iconURL.includes('fe-antd')) {
       skills.push({
         name: 'Ant Design',
@@ -20,11 +27,67 @@ export const FrontendSkillset = ({ frontendIcons }: any) => {
         alt: 'Ant Design Icon',
       });
     }
-    if (iconURL.includes('fe-figma')) {
+    if (iconURL.includes('fe-gatsby')) {
       skills.push({
-        name: 'Figma',
+        name: 'Gatsby',
         url: iconURL,
-        alt: 'Figma Icon',
+        alt: 'Gatsby Icon',
+      });
+    }
+    if (iconURL.includes('fe-less')) {
+      skills.push({
+        name: 'Less',
+        url: iconURL,
+        alt: 'Less Icon',
+      });
+    }
+    if (iconURL.includes('fe-jest')) {
+      skills.push({
+        name: 'Jest',
+        url: iconURL,
+        alt: 'Jest Icon',
+      });
+    }
+    if (iconURL.includes('fe-typescript')) {
+      skills.push({
+        name: 'Typescript',
+        url: iconURL,
+        alt: 'Typescript Icon',
+      });
+    }
+    if (iconURL.includes('fe-apollo')) {
+      skills.push({
+        name: 'Apollo Graphql',
+        url: iconURL,
+        alt: 'Apollo Graphql Icon',
+      });
+    }
+    if (iconURL.includes('fe-postman')) {
+      skills.push({
+        name: 'Postman/Newman',
+        url: iconURL,
+        alt: 'Postman Icon',
+      });
+    }
+    if (iconURL.includes('fe-lighthouse')) {
+      skills.push({
+        name: 'Lighthouse',
+        url: iconURL,
+        alt: 'Lighthouse Icon',
+      });
+    }
+    if (iconURL.includes('fe-cypress')) {
+      skills.push({
+        name: 'Cypress',
+        url: iconURL,
+        alt: 'Cypress Icon',
+      });
+    }
+    if (iconURL.includes('fe-sentry')) {
+      skills.push({
+        name: 'Sentry',
+        url: iconURL,
+        alt: 'Sentry Icon',
       });
     }
   });
