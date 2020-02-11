6042237b01563da83404ee31c2801a023bf93ead
Gilbertly <gilbert@qubitping.com>
2020-02-11 12:10:21 +0300

    feat: add frontend skillset & update named query

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,16 @@
 import * as React from 'react';
 import { graphql } from 'gatsby';
 import { Header } from '../components/Header';
+import { FrontendSkillset } from '../components/FrontendSkillset';
 
 const IndexPage = (props: any) => {
   const avatarSvg = props.data.avatarSvg.publicURL;
+  const frontendIcons = props.data.frontendIcons.edges;
 
   return (
     <div className="page-index">
       <Header avatarSvg={avatarSvg} />
+      <FrontendSkillset frontendIcons={frontendIcons} />
     </div>
   );
 };
@@ -15,9 +18,20 @@ const IndexPage = (props: any) => {
 export default IndexPage;
 
 export const pageQuery = graphql`
-  query {
+  query IndexPageQuery {
     avatarSvg: file(relativePath: { eq: "img/avatarProfile.svg" }) {
       publicURL
+    }
+    frontendIcons: allFile(filter: { absolutePath: { regex: "/(fe-)/" } }) {
+      edges {
+        node {
+          childImageSharp {
+            fluid(maxWidth: 22, maxHeight: 22) {
+              src
+            }
+          }
+        }
+      }
     }
   }
 `;
