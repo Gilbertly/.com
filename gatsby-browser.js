2826b5256dd86f007a192f3cd7c11955a3a3912c
Gilbertly <gilbert@qubitping.com>
2020-04-29 13:20:43 +0300

    chore: remove inline explicit rule & always reload pwa

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,12 +1,5 @@
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
 import './src/styles/index.less';
 
 export const onServiceWorkerUpdateReady = () => {
-  const answer = window.confirm(`
-    This application has been updated.
-    Reload to display the latest version?
-  `);
-  if (answer === true) {
-    window.location.reload();
-  }
+  window.location.reload(true);
 };
