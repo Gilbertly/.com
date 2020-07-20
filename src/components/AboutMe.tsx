c7963e04964fab629b6fd98be98fb5a2a7edb1c0
Gilbertly <gilbert@qubitping.com>
2020-07-20 13:41:46 +0300

    chore: reduce wall of text

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -6,8 +6,6 @@ export const AboutMe = () => {
       <h2 className="title">About Me</h2>
       <h3>{`
       I'm a software and data engineer with an experience in end-to-end projects, based in Nairobi, Kenya.
-      My focus is mainly working with data & dev teams to automate and scale workflows while keeping
-      infrastructure healthy and secure.
       `}</h3>
       <h3>{`
       Nowadays my time and responsibilities are split between learning how to effectively become an
