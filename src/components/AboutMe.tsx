1c1eca5846ce8c022e8fb2ad6c0a00e2686503c5
Gilbertly <gilbert@qubitping.com>
2021-01-18 12:47:52 +0300

    chore: drop data eng., add current venture, strike potato

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -5,12 +5,13 @@ export const AboutMe = () => {
     <section className="section-aboutme">
       <h2 className="title">About Me</h2>
       <h3>{`
-      I'm a software and data engineer with an experience in end-to-end projects, based in Nairobi, Kenya.
+      I am a software engineer with an experience in end-to-end projects, based in Nairobi, Kenya.
       `}</h3>
       <h3>{`
       Nowadays my time and responsibilities are split between learning how to effectively become an
-      IndieMaker, demystifying predictions from data models, and catching-up with family/friends. potato.
-      `}</h3>
+      IndieMaker, demystifying predictions from data models, and catching-up with family/friends.
+      `}<span className="strikethrough">{'potato'}</span></h3>
+      <h3>{`Currently I am building serverless-first backends with JAMStack.`}</h3>
     </section>
   );
 };
